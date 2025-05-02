64a1d737b7cfc4a800969289dab48b6f7d80d6c1
Mitchell Conrad <Mitchell.conrad@gmail.com>
2025-05-01 22:16:23 -0400

    base app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Timer from "../app/Timer";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
+
+        <h1 className="text-4xl">breathe</h1>
+        <Timer />
+
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -49,10 +54,33 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
       </main>
+
+
+      
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
+        
+      <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://mconrad.tech/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+          <Image
+            aria-hidden
+            src="/icon.png"
+            alt="MC icon"
+            width={32}
+            height={32}
+            className="rounded-sm"
+
+          />
+          {/* Mitchell Conrad */}
+        </a>
+
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -96,7 +124,9 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
+
+
       </footer>
     </div>
   );
