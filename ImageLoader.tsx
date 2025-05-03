62c1974e2eddab9c5e85e1be17271cca9a3cf93f
Mitchell Conrad <Mitchell.conrad@gmail.com>
2025-05-02 20:32:12 -0400

    fix images

---
+++ ImageLoader.tsx
@@ -0,0 +1,3 @@
+export default function ImageLoader(src: string) {
+  return `https://https://mitchell-conrad-20.github.io/meditation/${src}`;
+}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  images: {
+    loader: "custom",
+    loaderFile: './ImageLoader.tsx'
+  },
+  // images: { unoptimized: true } // works on both local and server
 };
 
 export default nextConfig;
