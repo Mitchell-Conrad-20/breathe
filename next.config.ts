0e90b1b0380fb8aa78b6e4ee80a4aac017531e37
Mitchell Conrad <Mitchell.conrad@gmail.com>
2025-05-02 20:38:59 -0400

    fix images again

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  images: {
-    loader: "custom",
-    loaderFile: './ImageLoader.tsx'
-  },
-  // images: { unoptimized: true } // works on both local and server
+  // images: {
+  //   loader: "custom",
+  //   loaderFile: './ImageLoader.tsx'
+  // },
+  images: { unoptimized: true } // works on both local and server
 };
 
 export default nextConfig;
